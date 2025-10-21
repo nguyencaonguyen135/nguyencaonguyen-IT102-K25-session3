9f7509bcf848a605f9f61cefdd173df1796722db
nguyencaonguyen135 <nguyencaonguyen135@gmail.com>
2025-10-21 22:11:11 +0700

    bài 9 sửa lại

--- ex9.c
+++ ex9.c
@@ -3,15 +3,14 @@
 
 int main() {
     int n;
-    do {
+    float A;
         printf("Nhap n (>1): ");
         scanf("%d", &n);
-
-        if (n <= 1) {
-            printf("Loi: n phai la so nguyen lon hon 1. Vui long nhap lai.\n");
-        }
-    } while (n <= 1); 
-    double A = (sqrt(2 * n + 1) - 1.0) / 2.0;
+        
+	A = (1.0 / ((n-1) * n)) + 
+        (1.0 / (n * (n+1))) + 
+        (1.0 / ((n+1) * (n+2)));
+		
     printf("A = %.5f\n", A);
 
     return 0;
